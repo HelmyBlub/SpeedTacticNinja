5bdd7fba61e08428265d305462546d434e517859
Helmy <helmyblub@gmail.com>
2025-11-05 15:49:27 +0100

    zig build for steam command

--- build.zig
+++ build.zig
@@ -5,31 +5,26 @@ pub fn build(b: *std.Build) void {
     const target = b.standardTargetOptions(.{});
     const optimize: std.builtin.OptimizeMode = b.standardOptimizeOption(.{});
     // const optimize: std.builtin.OptimizeMode = .ReleaseFast;
-    const exe = b.addExecutable(.{
-        .name = "speedTacticNinja",
-        .root_source_file = b.path("src/main.zig"),
-        .target = target,
-        .optimize = optimize,
-    });
-    if (optimize != .Debug and target.result.os.tag == .windows) {
-        exe.subsystem = .Windows;
-    }
-
-    const run_cmd = b.addRunArtifact(exe);
-    run_cmd.step.dependOn(b.getInstallStep());
-    if (b.args) |args| {
-        run_cmd.addArgs(args);
-    }
-    const run_step = b.step("run", "Run the app");
-    run_step.dependOn(&run_cmd.step);
 
     const sdl_dep = b.dependency("sdl", .{
         .target = target,
         .optimize = optimize,
     });
     const sdl_lib = sdl_dep.artifact("SDL3");
-    exe.root_module.linkLibrary(sdl_lib);
-    b.installArtifact(exe);
+    const zigimg_dependency = b.dependency("zigimg", .{
+        .target = target,
+        .optimize = optimize,
+    });
+
+    const executableData = addExecutable(b, target, optimize, sdl_lib, zigimg_dependency, false);
+
+    const run_cmd = b.addRunArtifact(executableData.exe);
+    run_cmd.step.dependOn(b.getInstallStep());
+    if (b.args) |args| {
+        run_cmd.addArgs(args);
+    }
+    const run_step = b.step("run", "Run the app");
+    run_step.dependOn(&run_cmd.step);
 
     const unit_tests = b.addTest(.{
         .root_source_file = b.path("src/main.zig"),
@@ -41,14 +36,12 @@ pub fn build(b: *std.Build) void {
     const test_step = b.step("test", "Run unit tests");
     test_step.dependOn(&run_unit_tests.step);
 
-    const zigimg_dependency = b.dependency("zigimg", .{
-        .target = target,
-        .optimize = optimize,
-    });
-    compileShared(exe, zigimg_dependency, b);
     compileShared(unit_tests, zigimg_dependency, b);
-    // this just adds the dll file to zig-out/bin dir
-    b.installBinFile("dependencies/steam_api64.dll", "steam_api64.dll");
+    steamSetupStep(
+        b,
+        target,
+        optimize,
+    );
 }
 
 fn compileShared(compile: *std.Build.Step.Compile, zigimg: *std.Build.Dependency, b: *std.Build) void {
@@ -63,4 +56,72 @@ fn compileShared(compile: *std.Build.Step.Compile, zigimg: *std.Build.Dependency
     compile.addCSourceFile(.{ .file = b.path("dependencies/volk.c") });
 
     compile.root_module.addImport("zigimg", zigimg.module("zigimg"));
+}
+
+fn addExecutable(b: *std.Build, target: std.Build.ResolvedTarget, optimize: std.builtin.OptimizeMode, sdl_lib: *std.Build.Step.Compile, zigimg: *std.Build.Dependency, skipInstall: bool) struct {
+    exe: *std.Build.Step.Compile,
+    artifact: *std.Build.Step.InstallArtifact,
+} {
+    const exe = b.addExecutable(.{
+        .name = "speedTacticNinja",
+        .root_source_file = b.path("src/main.zig"),
+        .target = target,
+        .optimize = optimize,
+    });
+    const install_step = b.addInstallArtifact(exe, .{});
+    if (target.result.os.tag == .windows) {
+        if (optimize != .Debug) {
+            exe.subsystem = .Windows;
+        }
+        install_step.dest_dir = .{ .custom = "steam/windows" };
+        // this just adds the dll file to zig-out/bin dir
+        b.getInstallStep().dependOn(&b.addInstallFileWithDir(b.path("dependencies/steam_api64.dll"), .{ .custom = "steam/windows" }, "steam_api64.dll").step);
+    } else {
+        install_step.dest_dir = .{ .custom = "steam/linux" };
+        b.getInstallStep().dependOn(&b.addInstallFileWithDir(b.path("dependencies/libsteam_api.so"), .{ .custom = "steam/linux" }, "libsteam_api.so").step);
+    }
+    if (!skipInstall) b.getInstallStep().dependOn(&install_step.step);
+    exe.root_module.linkLibrary(sdl_lib);
+    compileShared(exe, zigimg, b);
+    return .{ .exe = exe, .artifact = install_step };
+}
+
+fn steamSetupStep(b: *std.Build, target: std.Build.ResolvedTarget, optimize: std.builtin.OptimizeMode) void {
+    const target2 = if (target.result.os.tag == .windows)
+        b.resolveTargetQuery(.{ .cpu_arch = .x86_64, .os_tag = .linux, .abi = .gnu })
+    else
+        b.resolveTargetQuery(.{ .cpu_arch = .x86_64, .os_tag = .windows });
+
+    const sdl_dep = b.dependency("sdl", .{
+        .target = target2,
+        .optimize = optimize,
+    });
+    const sdl_lib = sdl_dep.artifact("SDL3");
+    const zigimg_dependency = b.dependency("zigimg", .{
+        .target = target2,
+        .optimize = optimize,
+    });
+    const executableData2 = addExecutable(b, target2, optimize, sdl_lib, zigimg_dependency, true);
+    const steam_step = b.step("steam", "copy files");
+    steam_step.dependOn(b.getInstallStep());
+    steam_step.dependOn(&executableData2.artifact.step);
+    const install_data_sounds = b.addInstallDirectory(.{
+        .source_dir = b.path("sounds"),
+        .install_dir = .prefix,
+        .install_subdir = "steam/all/sounds",
+    });
+    steam_step.dependOn(&install_data_sounds.step);
+    const install_data_shaders = b.addInstallDirectory(.{
+        .source_dir = b.path("shaders"),
+        .install_dir = .prefix,
+        .install_subdir = "steam/all/shaders",
+    });
+    steam_step.dependOn(&install_data_shaders.step);
+    const install_data_images = b.addInstallDirectory(.{
+        .include_extensions = &[1][]const u8{"png"},
+        .source_dir = b.path("images"),
+        .install_dir = .prefix,
+        .install_subdir = "steam/all/images",
+    });
+    steam_step.dependOn(&install_data_images.step);
 }
